9ba8693e3049213e85549c3c9b62ca7a40667778
Isabella Becker <isabella.becker@edu.pucrs.br>
2022-06-08 17:28:10 -0300

    tentativa leitura de arquivo

--- index.c
+++ index.c
@@ -10,19 +10,22 @@ void usage ()
 {
     printf("USAGE FUNCIONA?");
 }
+void file()
+{
+}
 
 int main (int argc, char **argv)
 {
-
     int i;
     for (i = 0; i < argc; i ++)
     {
-        printf("%d- Parâmetro = \"%s\"\n", i + 1, argv[i]);
+        printf("%d- Parametro = \"%s\"\n", i + 1, argv[i]);
     }
 
+    
     int op;
 
-    while ((op = getopt(argc, argv, "uhe")) != EOF)
+    while ((op = getopt(argc, argv, "uhf")) != EOF)
     {
         switch (op)
         {
@@ -31,6 +34,24 @@ int main (int argc, char **argv)
                 break; 
             case 'h':
                 help();
+                break;
+            case 'f':
+                FILE *file;
+                file = fopen("%s",argv[2]);
+
+                    if(file == NULL) 
+                {
+                    printf("Nao foi possivel abrir o arquivo.\n");
+                    getchar();  
+                    exit(0);
+                }
+
+                char frase[8];
+                
+                while(fgets(frase, 8, file) != NULL)
+                {
+                    printf("%s", frase);
+                }
                 break;
             default:
                 exit(0);
