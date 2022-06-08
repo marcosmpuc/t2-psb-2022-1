0fb9039d014ae999bb5d685c9a32aca1b7ac41d5
Marcos MS <marcos.m@edu.pucrs.br>
2022-06-08 13:36:57 -0300

    Teste

---
+++ index.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void help ()
+{
+    printf("HEEEEELP!!!!");
+}
+
+void usage ()
+{
+    printf("USAGE FUNCIONA?");
+}
+
+int main (int argc, char **argv)
+{
+
+    int i;
+    for (i = 0; i < argc; i ++)
+    {
+        printf("%d- Parâmetro = \"%s\"\n", i + 1, argv[i]);
+    }
+
+    int op;
+
+    while ((op = getopt(argc, argv, "uhe")) != EOF)
+    {
+        switch (op)
+        {
+            case 'u':
+                usage();
+                break; 
+            case 'h':
+                help();
+            default:
+                exit(0);
+                break;
+        }
+    }
+
+    return 0;
+}
