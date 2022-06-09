08c6a0d3bc5f7c234532b3c5ab8db7b5db570e76
Marcos MS <marcos.m@edu.pucrs.br>
2022-06-09 13:58:45 -0300

    Recebe comandos a partir do terminal: é possível informar se se deseja escrever no terminal, em um txt, ou em um HTML, ou em qualquer combinação destes.

--- index.c
+++ index.c
@@ -8,31 +8,42 @@
 static char line_buffer[BUFFERSIZE];
 //static char *paragraph_number_last_digit = line_buffer[BUFFERSIZE - 1];
 
-void help ()
+void version (void)
 {
-    printf("HEEEEELP!!!!");
+    printf("\
+    EN-US: Index generator - Versão 0.3.12\n\
+    PT-BR: Gerador de índice remissivo - Versão 0.3.12\n\
+    \n\
+    -u to view usage options\n\
+    -u para visualizar opções de visualização");
 }
 
-void usage ()
+void usage (void)
 {
-    printf("USAGE FUNCIONA?");
+    printf("\
+    EN-US: Index generator command options - Versão 0.3.12\n\
+    PT-BR: Opções de comando do gerador de índice remissivo - Versão 0.3.12\n\
+    \n\
+    -u to view usage options\n\
+    -u para visualizar opções de visualização\n\
+    \n\
+    -s to view index entries in terminal (shell)\n\
+    -s para visualizar registros do índice no terminal (shell)\n\
+    \n\
+    -h to generate HTML file\n\
+    -h para gerar arquivo HTML\n\
+    \n\
+    -t to generate .txt file\n\
+    -t para gerar arquivo .txt");
 }
 
-void addParagraphInfo (int position_to_write_at, FILE *outputTXT, FILE *outputHTML)
+void addParagraphInfo (int position_to_write_at, FILE *output)
 {
-    char sentence[12] = {':', ' ', 'p', 'a', 'r', 'a', 'g', 'r', 'a', 'f', 'o', ' '};
-    for (int i = 0; i < 12; i++)
+    char sentence[10] = {'p', 'a', 'r', 'a', 'g', 'r', 'a', 'f', 'o', ' '};
+    for (int i = 0; i < 10; i++)
     {
-        fputc(sentence[i], outputTXT);
-        if (i > 1) 
-            fputc(sentence[i], outputHTML);
+        fputc(sentence[i], output);
     }
-    
-    fputc('1', outputTXT);
-    fputc('1', outputHTML);
-    fputc('\n', outputTXT);
-    fputc('\n', outputHTML);
-
 }
 
 /*char* paragraphIncrement (char digits[])
@@ -57,7 +68,7 @@ void addParagraphInfo (int position_to_write_at, FILE *outputTXT, FILE *outputHT
     }
 }*/
 
-void processing (void)
+void processing (int print_on_terminal, int print_in_html, int print_in_txt)
 {
     FILE *input = fopen("input.txt", "r+");
     FILE *outputTXT  = fopen("output.txt", "w+");
@@ -69,7 +80,7 @@ void processing (void)
         int n = 0;
         //char paragraphCount[4] = { NULL, NULL, NULL, '0'};
         int has_to_add = 0;
-        int paragraph_number = 1;
+        char paragraph_number = '1';
 
         while(test = line_buffer[n])
         {
@@ -83,16 +94,23 @@ void processing (void)
             {
                 if(has_to_add == 0)
                 {
-                    fputc('<', outputHTML);
-                    fputc('h', outputHTML);
-                    fputc('1', outputHTML);
-                    fputc('>', outputHTML);
+                    if (print_in_html)
+                    {
+                        fputc('<', outputHTML);
+                        fputc('h', outputHTML);
+                        fputc('1', outputHTML);
+                        fputc('>', outputHTML);
+                    }
                     has_to_add = 1;
                 }
                 
-                printf("%c", test);
-                fputc(test, outputTXT);
-                fputc(test, outputHTML);
+                if (print_on_terminal)
+                    printf("%c", test);
+                if (print_in_txt)
+                    fputc(test, outputTXT);
+                if (print_in_html)
+                    fputc(test, outputHTML);
+                
                 n++;
             }
             else
@@ -100,20 +118,34 @@ void processing (void)
                 if (has_to_add)
                 {
                     n++;
-                    fputc('<', outputHTML);
-                    fputc('/', outputHTML);
-                    fputc('h', outputHTML);
-                    fputc('1', outputHTML);
-                    fputc('>', outputHTML);
-                    fputc('<', outputHTML);
-                    fputc('p', outputHTML);
-                    fputc('>', outputHTML);
-                    printf(": parágrafo %d\n", paragraph_number);
-                    addParagraphInfo(n, outputTXT, outputHTML);
-                    fputc('<', outputHTML);
-                    fputc('/', outputHTML);
-                    fputc('p', outputHTML);
-                    fputc('>', outputHTML);
+                    if (print_on_terminal)
+                        printf(": parágrafo %d\n", paragraph_number);
+                    if(print_in_html)
+                    {
+                        fputc('<', outputHTML);
+                        fputc('/', outputHTML);
+                        fputc('h', outputHTML);
+                        fputc('1', outputHTML);
+                        fputc('>', outputHTML);
+                        fputc('<', outputHTML);
+                        fputc('p', outputHTML);
+                        fputc('>', outputHTML);
+                        addParagraphInfo(n, outputHTML);
+                        fputc(paragraph_number, outputHTML);
+                        fputc('<', outputHTML);
+                        fputc('/', outputHTML);
+                        fputc('p', outputHTML);
+                        fputc('>', outputHTML);
+                        fputc('\n', outputHTML);
+                    }
+                    if (print_in_txt)
+                    {
+                        fputc(':', outputTXT);
+                        fputc(' ', outputTXT);
+                        addParagraphInfo(n, outputTXT);
+                        fputc(paragraph_number, outputTXT);
+                        fputc('\n', outputTXT);
+                    }
                     
                     has_to_add = 0;
                 }
@@ -128,7 +160,6 @@ void processing (void)
 
 int main (int argc, char **argv)
 {
-    processing();
     int i;
     for (i = 0; i < argc; i ++)
     {
@@ -136,21 +167,34 @@ int main (int argc, char **argv)
     }
 
     int op;
+    int print_on_terminal = 0; 
+    int print_in_html = 0;
+    int print_in_txt = 0;
 
-    while ((op = getopt(argc, argv, "uhe")) != EOF)
+    while ((op = getopt(argc, argv, "ushte")) != EOF)
     {
         switch (op)
         {
             case 'u':
                 usage();
-                break; 
+                break;
+            case 's':
+                print_on_terminal = 1;
+                break;
             case 'h':
-                help();
+                print_in_html = 1;
                 break;
+            case 't':
+                print_in_txt = 1;
+                break;
+            case 'e':
+                return 0;
             default:
-                exit(0);
-                break;
+                version();
         }
     }
+
+    processing(print_on_terminal, print_in_html, print_in_txt);
+
     return 0;
 }
