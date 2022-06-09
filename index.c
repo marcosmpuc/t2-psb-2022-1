603c6d528a6bafb56c68f653809410c5dec45009
Marcos MS <marcos.m@edu.pucrs.br>
2022-06-09 00:48:55 -0300

    Escreve com sucesso no arquivo de saída

--- index.c
+++ index.c
@@ -18,12 +18,10 @@ void usage ()
 
 void processing (void)
 {
-    /*
-    FILE *input;
-    input = fopen("input.txt", "r");
-    FILE *output  = fopen("output.txt", "w");
+    FILE *input = fopen("input.txt", "r+");
+    FILE *output  = fopen("output.txt", "w+");
 
-    if(!input) printf("Não abriu entrada\n");
+    /*if(!input) printf("Não abriu entrada\n");
     if(!output) printf("Não abriu saída\n");
     
     char buffer[BUFFERSIZE];
@@ -34,11 +32,42 @@ void processing (void)
     }
     */
     
-    char* buffer; printf("DECLARAÇÃO: POINTER DE CARACTERE buffer\n");
+    char buffer[BUFFERSIZE]; printf("DECLARAÇÃO: POINTER DE CARACTERE buffer\n");
 
-    while (fgets(buffer,106,INPUT))
+    fgets(buffer, BUFFERSIZE, input);
+    char test;
+    int n = 0;
+    int paragraphCount = 0;
+
+    while(test = buffer[n])
     {
         printf("TENTOU LER BYTES\n");
+        if(test == '\0')
+            break;
+        if ((test == 39)
+            || (test >= 65 && test <= 90)
+            || (test >= 97 && test <= 122))
+        {
+            printf("ACHOU CARACTERES ÚTEIS\n");
+            fputc(test, output); printf("ESCREVEU BYTES\n");
+        }
+        else
+        {
+            printf("NÃO ACHOU CARACTERE ÚTIL\n");
+            char sentence[12] = {',', ' ', 'p', 'a', 'r', 'a', 'g', 'r', 'a', 'f', 'o', ' '};
+            for (int i = 0; i < 12; i++)
+                fputc(sentence[i], output);
+            // escreve número do parágrafo
+            fputc('\n', output);
+        }
+        n++;
+    }
+
+    /*while (fgets(buffer, 106, INPUT))
+    {
+        printf("TENTOU LER BYTES\n");
+        if(*buffer == '\0')
+            break;
         if ((*buffer == 39)
             || (*buffer >= 65 && *buffer <= 90)
             || (*buffer >= 97 && *buffer <= 122))
@@ -51,7 +80,7 @@ void processing (void)
             printf("NÃO ACHOU CARACTERE ÚTIL\n");
             fputc(buffer, OUTPUT);
         }
-    }
+    }*/
     /*
     int n = 0;
 
