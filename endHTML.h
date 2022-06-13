d0532508b58cf6f98bf886ad2326893792f2ee95
Marcos MS <marcos.m@edu.pucrs.br>
2022-06-13 13:54:42 -0300

    Remove arquivos de cabeçalho não mais usados.

--- endHTML.h
+++
@@ -1,6 +0,0 @@
-#ifndef END_HTML_H_
-#   define END_HTML_H_
-
-    void end_html (FILE *output);
-
-#endif
--- startHTML.h
+++
@@ -1,6 +0,0 @@
-#ifndef START_HTML_H_
-#   define START_HTML_H_
-
-    void start_html (FILE *output);
-
-#endif
